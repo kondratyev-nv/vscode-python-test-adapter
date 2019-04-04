f705f36e19b55abd23cea614c57453ec29c904ef
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2019-04-04 21:11:46 +0300

    Fix cancellation tests

--- src/unittest/unittestScripts.ts
+++ src/unittest/unittestScripts.ts
@@ -13,16 +13,46 @@ TEST_RESULT_PREFIX = '${TEST_RESULT_PREFIX}'
 STDOUT_LINE = '\\nStdout:\\n%s'
 STDERR_LINE = '\\nStderr:\\n%s'
 
+
+def write_test_state(stream, state, result):
+    message = base64.b64encode(result[1].encode('utf8')).decode('ascii')
+    stream.writeln()
+    stream.writeln("{}:{}:{}:{}".format(TEST_RESULT_PREFIX,
+                                        state, result[0].id(), message))
+
+
 class TextTestResultWithSuccesses(TextTestResult):
     def __init__(self, *args, **kwargs):
         super(TextTestResultWithSuccesses, self).__init__(*args, **kwargs)
         self.successes = []
+        self.writtenResults = set()
 
     # similar to how unittest.TestResult done capturing
     def addSuccess(self, test):
         super(TextTestResultWithSuccesses, self).addSuccess(test)
         self.successes.append((test, self._execution_info_to_string(test)))
         self._mirrorOutput = True
+        write_test_state(self.stream, "passed", self.successes[-1])
+
+    def addError(self, test, err):
+        super(TextTestResultWithSuccesses, self).addError(test, err)
+        write_test_state(self.stream, "failed", self.errors[-1])
+
+    def addFailure(self, test, err):
+        super(TextTestResultWithSuccesses, self).addFailure(test, err)
+        write_test_state(self.stream, "failed", self.failures[-1])
+
+    def addSkip(self, test, reason):
+        super(TextTestResultWithSuccesses, self).addSkip(test, reason)
+        write_test_state(self.stream, "skipped", self.skipped[-1])
+
+    def addExpectedFailure(self, test, err):
+        super(TextTestResultWithSuccesses, self).addExpectedFailure(test, err)
+        write_test_state(self.stream, "passed", self.expectedFailures[-1])
+
+    def addUnexpectedSuccess(self, test):
+        super(TextTestResultWithSuccesses, self).addUnexpectedSuccess(test)
+        write_test_state(self.stream, "failed", self.unexpectedSuccesses[-1])
 
     def _execution_info_to_string(self, test):
         msgLines = []
@@ -38,15 +68,6 @@ class TextTestResultWithSuccesses(TextTestResult):
                     error += '\\n'
                 msgLines.append(STDERR_LINE % error)
         return ''.join(msgLines)
-
-
-class TextTestRunnerWithSingleResult(TextTestRunner):
-    def __init__(self, *args, **kwargs):
-        super(TextTestRunnerWithSingleResult, self).__init__(*args, buffer = True, **kwargs)
-        self.single_result = TextTestResultWithSuccesses(self.stream, self.descriptions, self.verbosity)
-
-    def _makeResult(self):
-        return self.single_result
 
 
 def get_tests(suite):
@@ -69,23 +90,12 @@ def filter_by_test_ids(tests, test_ids):
     return filter(lambda test: any(test.id().startswith(name) for name in test_ids), tests)
 
 
-def write_test_state(state, result):
-    message = base64.b64encode(result[1].encode('utf8')).decode('ascii')
-    print("{}:{}:{}:{}".format(TEST_RESULT_PREFIX, state, result[0].id(), message))
-
-
 def run_tests(start_directory, pattern, test_names):
-    runner = TextTestRunnerWithSingleResult()
-    tests = [test for test in filter_by_test_ids(discover_tests(start_directory, pattern), test_names)]
+    runner = TextTestRunner(
+        buffer=True, resultclass=TextTestResultWithSuccesses, stream=sys.stdout)
+    tests = filter_by_test_ids(discover_tests(
+        start_directory, pattern), test_names)
     result = runner.run(TestSuite(tests))
-    for r in result.skipped:
-        write_test_state("skipped", r)
-    for r in result.failures:
-        write_test_state("failed", r)
-    for r in result.errors:
-        write_test_state("failed", r)
-    for r in result.successes:
-        write_test_state("passed", r)
 
 
 action = sys.argv[1]

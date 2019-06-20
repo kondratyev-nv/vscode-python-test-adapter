68c31c666de9adbfcf65994974857cbae4ca1ed5
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2019-06-20 23:09:32 +0300

    Try dotenv-expand

--- test/test_samples/pytest/test/env_variables_test.py
+++ test/test_samples/pytest/test/env_variables_test.py
@@ -3,7 +3,7 @@ import os
 
 def test_environment_variable_from_env_file_passed():
     assert 'SOME_FILE_VARIABLE' in os.environ
-    assert os.getenv('SOME_FILE_VARIABLE') == 'HelloFromEnvFile'
+    assert os.getenv('SOME_FILE_VARIABLE') == ':1:2:3'
 
 
 def test_environment_variable_from_process_passed():
